b85cc68da28824759b718d99405ed1bfd40c7e95
cr6588 <cr6588@vip.qq.com>
2018-08-08 17:16:26 +0800

    complete Output Management

--- webpack-demo/Output Management/src/index.js
+++ webpack-demo/Output Management/src/index.js
@@ -1,11 +1,18 @@
-import _ from 'lodash';
+  import _ from 'lodash';
+  import printMe from './print.js';
 
-function component() {
-  let element = document.createElement('div');
+  function component() {
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+ 
+    element.appendChild(btn);
 
-document.body.appendChild(component());
+    return element;
+  }
+
+  document.body.appendChild(component());
